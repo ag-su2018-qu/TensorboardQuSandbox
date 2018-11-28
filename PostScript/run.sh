6aa635283f00f0d9250065b629762dadf6fb7b9b
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-11-28 14:39:55 -0500

    Removing absolute path

--- PostScript/run.sh
+++ PostScript/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/jovyan/work/FinanceCases/timeseries/Case_study/Swap_rate_forecast
+cd FinanceCases/timeseries/Case_study/Swap_rate_forecast
 nohup tensorboard --logdir='.' --port 7920 &
