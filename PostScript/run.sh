1d69b6909afea13ffe34428c4029b28b51c9d640
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-11-28 15:40:56 -0500

    Adding docker script

--- PostScript/run.sh
+++ PostScript/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd FinanceCases/timeseries/Case_study/Swap_rate_forecast
-nohup tensorboard --logdir='.' --port 7920 &
+i=$(sudo docker ps -aqf "name=project")
+sudo docker exec $i /bin/sh -c 'cd home/jovyan/**/FinanceCases/timeseries/Case_study/Swap_rate_forecast ; nohup tensorboard --logdir='.' --port 7920 &'
