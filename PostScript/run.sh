8243d72ede74b4c36c73286a7fac39e7a2fd2e31
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-11-28 16:27:15 -0500

    Fixing nohup command

--- PostScript/run.sh
+++ PostScript/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 i=$(sudo docker ps -aqf "name=project")
-sudo docker exec $i /bin/sh -c 'cd /home/jovyan/**/FinanceCases/timeseries/Case_study/Swap_rate_forecast ; nohup tensorboard --logdir='.' --port 7920 &'
+sudo docker exec $i /bin/sh -c 'cd /home/jovyan/**/FinanceCases/timeseries/Case_study/Swap_rate_forecast ; nohup tensorboard --logdir='.' --port 7920 > /dev/null 2>&1 &'
