0b7b68518cebf5c7b1547d63f31e5ca83888073f
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-11-28 18:04:43 -0500

    Updating post script

--- PostScript/run.sh
+++ PostScript/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 i=$(sudo docker ps -aqf "name=project")
-sudo docker exec $i /bin/sh -c 'cd /home/jovyan/**/FinanceCases/timeseries/Case_study/Swap_rate_forecast ; nohup tensorboard --logdir='.' --port 7920 > /dev/null 2>&1 &'
+sudo docker exec $i /bin/sh -c 'cd /home/jovyan/**/TimeSeries/Case_study/Swap_rate_forecast ; nohup tensorboard --logdir='.' --port 7920 > /dev/null 2>&1 &'
+sudo docker exec $i /bin/sh -c 'cd /home/jovyan/**/TimeSeries/Case_study/Temp_Anomalies ; nohup tensorboard --logdir='.' --port 7940 > /dev/null 2>&1 &'
